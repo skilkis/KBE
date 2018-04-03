358c0d89963ca43b26ae5e2bdd5931813ce03d36
nlsnjohnson <nlsnjohnson@gmail.com>
2018-04-03 11:43:20 +0200

    Wing Power Loading Script

---
+++ WingThrustLoading.py
@@ -0,0 +1,6 @@
+#This class will perform the Class I weight estimation for the fixed wing electric drone
+#based on the input MTOW.
+#There are assumed values for: C_lmax, Stall speed, propellor efficiency,
+#zero-lift drag coefficient, Aspect Ratio and Oswald Efficiency Factor.
+#Also, the climb rate and climb gradient must be assumed.
+
